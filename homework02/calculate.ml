bc5794728eb4ab920e21670e040c59d1033c0e50
Leeingnyo <dyd1928@naver.com>
2019-09-17 16:51:16 +0900

    2-1 fix bug... completed!

--- homework02/calculate.ml
+++ homework02/calculate.ml
@@ -39,19 +39,31 @@ let rec build e = match e with
     | (VALUE _a, VALUE _b) -> VALUE (fun x -> eval (build a, x) /. eval (build b, x))
     | (_, _) -> FORMULA (fun x -> eval (build a, x) /. eval (build b, x))
     )
-  | SIGMA (a, b, f) -> (VALUE (fun x ->
-    let ea = int_of_float (eval (build a, x)) in
-    let eb = int_of_float (eval (build b, x)) in
-    if ea > eb then 0. else (eval ((build f), (eval (build a, x))) +.
-        (eval (build (SIGMA (INT (ea + 1), INT eb, f)), x)))
-    ))
-  | INTEGRAL (a, b, f) -> VALUE (fun x ->
-    let ea = (eval (build a, x)) in
-    let eb = (eval (build b, x)) in
-    if ea > eb then -1. *. eval (build (INTEGRAL (b, a, f)), x)
-    else if ea = eb then 0.
-    else (eval (build f, ea) *. 0.1 +. eval (build (INTEGRAL (REAL (0.1 +. ea), b, f)), x))
+  | SIGMA (a, b, f) -> (match (build a, build b) with
+    | (VALUE _a, VALUE _b) -> VALUE (fun x ->
+      let ea = int_of_float (eval (build a, x)) in
+      let eb = int_of_float (eval (build b, x)) in
+      if ea > eb then 0. else (eval ((build f), (eval (build a, x))) +.
+          (eval (build (SIGMA (INT (ea + 1), INT eb, f)), x))))
+    | (_, _) -> FORMULA (fun x ->
+      let ea = int_of_float (eval (build a, x)) in
+      let eb = int_of_float (eval (build b, x)) in
+      if ea > eb then 0. else (eval ((build f), (eval (build a, x))) +.
+          (eval (build (SIGMA (INT (ea + 1), INT eb, f)), x))))
     )
+  | INTEGRAL (a, b, f) -> match (build a, build b) with
+    | (VALUE _a, VALUE _b) -> VALUE (fun x ->
+      let ea = (eval (build a, x)) in
+      let eb = (eval (build b, x)) in
+      if ea > eb then -1. *. eval (build (INTEGRAL (b, a, f)), x)
+      else if ea +. 0.1 > eb then 0. (* ??? *)
+      else (eval (build f, ea) *. 0.1 +. eval (build (INTEGRAL (REAL (0.1 +. ea), b, f)), x)))
+    | (_, _) -> FORMULA (fun x ->
+      let ea = (eval (build a, x)) in
+      let eb = (eval (build b, x)) in
+      if ea > eb then -1. *. eval (build (INTEGRAL (b, a, f)), x)
+      else if ea +. 0.1 > eb then 0. (* ??? *)
+      else (eval (build f, ea) *. 0.1 +. eval (build (INTEGRAL (REAL (0.1 +. ea), b, f)), x)))
 
 (* exp -> float *)
 let calculate e = match (build e) with
