ee02db0ea7970e68323a45db24d31342e68db62b
Leeingnyo <dyd1928@naver.com>
2019-11-05 14:22:20 +0900

    add sm5 skel, implement some expression

---
+++ homework06/SM5_skel/translate.ml
@@ -0,0 +1,31 @@
+(*
+ * SNU 4190.310 Programming Languages 
+ * K-- to SM5 translator skeleton code
+ * DongKwon Lee (dklee@ropas.snu.ac.kr)
+ *)
+
+open K
+open Sm5
+module Translator = struct
+
+  (* TODO : complete this function  *)
+  let rec trans : K.program -> Sm5.command = function
+    | K.NUM i -> [Sm5.PUSH (Sm5.Val (Sm5.Z i))]
+    | K.TRUE -> [Sm5.PUSH (Sm5.Val (Sm5.B true))]
+    | K.FALSE -> [Sm5.PUSH (Sm5.Val (Sm5.B false))]
+    | K.VAR id -> [Sm5.PUSH (Sm5.Id id); Sm5.LOAD]
+    | K.ADD (e1, e2) -> trans e1 @ trans e2 @ [Sm5.ADD]
+    | K.SUB (e1, e2) -> trans e1 @ trans e2 @ [Sm5.SUB]
+    | K.MUL (e1, e2) -> trans e1 @ trans e2 @ [Sm5.MUL]
+    | K.DIV (e1, e2) -> trans e1 @ trans e2 @ [Sm5.DIV]
+    | K.EQUAL (e1, e2) -> trans e1 @ trans e2 @ [Sm5.EQ]
+    | K.LESS (e1, e2) -> trans e1 @ trans e2 @ [Sm5.LESS]
+    | K.NOT e1 -> trans e1 @ [Sm5.NOT]
+    | K.READ x -> [Sm5.GET; Sm5.PUSH (Sm5.Id x); Sm5.STORE; Sm5.PUSH (Sm5.Id x); Sm5.LOAD]
+    | K.WRITE e -> trans e @ [Sm5.PUT]
+    | K.LETV (x, e1, e2) ->
+      trans e1 @ [Sm5.MALLOC; Sm5.BIND x; Sm5.PUSH (Sm5.Id x); Sm5.STORE] @
+      trans e2 @ [Sm5.UNBIND; Sm5.POP]
+    | _ -> failwith "Unimplemented"
+
+end
