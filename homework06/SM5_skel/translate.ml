d5556186f8ecb1f90d2828ff0aae2fb88ab0007d
Leeingnyo <dyd1928@naver.com>
2019-11-07 15:40:19 +0900

    i dont know...

--- homework06/SM5_skel/translate.ml
+++ homework06/SM5_skel/translate.ml
@@ -23,23 +23,25 @@ module Translator = struct
     | K.LESS (e1, e2) -> trans e1 @ trans e2 @ [Sm5.LESS]
     | K.NOT e1 -> trans e1 @ [Sm5.NOT]
     | K.READ x -> [Sm5.GET; Sm5.PUSH (Sm5.Id x); Sm5.STORE; Sm5.PUSH (Sm5.Id x); Sm5.LOAD]
-    | K.WRITE e -> trans e @ [Sm5.PUT]
+    | K.WRITE e -> trans e @ [Sm5.MALLOC; Sm5.BIND "write"; Sm5.PUSH (Sm5.Id "write"); Sm5.STORE; Sm5.PUSH (Sm5.Id "write"); Sm5.LOAD; Sm5.PUSH (Sm5.Id "write"); Sm5.LOAD; Sm5.PUT; Sm5.UNBIND; Sm5.POP]
     | K.LETV (x, e1, e2) ->
       trans e1 @ [Sm5.MALLOC; Sm5.BIND x; Sm5.PUSH (Sm5.Id x); Sm5.STORE] @
       trans e2 @ [Sm5.UNBIND; Sm5.POP]
     | K.LETF (f, x, e1, e2) ->
       [Sm5.PUSH (Sm5.Fn (x, trans e1)); Sm5.BIND f] @
-      trans e2 @ [Sm5.UNBIND]
-    | K.ASSIGN (x, e) -> trans e @ [Sm5.PUSH (Sm5.Id x); Sm5.STORE]
+      trans e2 @ [Sm5.UNBIND; Sm5.POP]
+    | K.ASSIGN (x, e) -> trans e @ [Sm5.MALLOC; Sm5.BIND "assign"; Sm5.PUSH (Sm5.Id "assign"); Sm5.STORE; Sm5.PUSH (Sm5.Id "assign"); Sm5.LOAD; Sm5.PUSH (Sm5.Id "assign"); Sm5.LOAD; Sm5.PUSH (Sm5.Id x); Sm5.STORE; Sm5.UNBIND; Sm5.POP]
     | K.IF (e_cond, e_true, e_false) ->
       trans e_cond @ [Sm5.JTR (trans e_true, trans e_false)]
     | K.WHILE (e_cond, e_body) -> failwith "Unimplemented WHILE"
     | K.FOR (id, e1, e2, e_body) -> failwith "Unimplemented FOR"
     | K.SEQ (e1, e2) -> trans e1 @ trans e2
     | K.CALLV (f, arg_exp) ->
-      [Sm5.PUSH (Sm5.Id f)] @ trans arg_exp @ [Sm5.MALLOC; Sm5.CALL]
+      [Sm5.PUSH (Sm5.Id f)] @
+      trans arg_exp @ [Sm5.MALLOC; Sm5.CALL; Sm5.POP] @
+      []
     | K.CALLR (f, arg_var) ->
-      [Sm5.PUSH (Sm5.Id f); Sm5.PUSH (Sm5.Id arg_var); Sm5.LOAD; Sm5.PUSH (Sm5.Id arg_var); Sm5.CALL]
+      [Sm5.PUSH (Sm5.Id f); Sm5.PUSH (Sm5.Id arg_var); Sm5.LOAD; Sm5.PUSH (Sm5.Id arg_var); Sm5.CALL; Sm5.POP]
     | _ -> failwith "Unimplemented"
 
 end
