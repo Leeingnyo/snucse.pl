8b6bf899d20e47e35e76622595d690a81f40cc85
Leeingnyo <dyd1928@naver.com>
2019-12-07 00:02:35 +0900

    add exn file, implement desugar

---
+++ homework08/Exn_skel/desugar.ml
@@ -0,0 +1,31 @@
+(*
+ * SNU 4190.310 Programming Languages 
+ * Homework "Exceptions are sugar" Skeleton
+ *)
+
+open Xexp
+
+let count = ref 0
+
+let new_name () = 
+  let _ = count := !count + 1 in
+  "x_" ^ (string_of_int !count)
+
+let removeExnIter : xexp -> xexp = fun e ->
+  let k = new_name () in
+  let h = new_name () in
+  match e with
+  | Xexp.Num n -> Xexp.Fn (k, Xexp.Fn (h, Xexp.App (Xexp.Var k, Xexp.Num n)))
+  | Xexp.Var id -> Xexp.Fn (k, Xexp.Fn (h, Xexp.App (Xexp.Var k, Xexp.Var id)))
+  | Xexp.Fn (f, e) -> Xexp.Fn (k, Xexp.Fn (h, Xexp.App (Xexp.Var k, e)))
+  | Xexp.App (fn, arg) -> Xexp.Fn (k, Xexp.Fn (h, Xexp.App (Xexp.Var k, e)))
+  | Xexp.If (cond, t, f) -> Xexp.Fn (k, Xexp.Fn (h, Xexp.App (Xexp.Var k, e)))
+  | Xexp.Equal (left, right) ->  Xexp.Fn (k, Xexp.Fn (h, Xexp.App (Xexp.Var k, e)))
+  | Xexp.Raise expn -> Xexp.Fn (k, Xexp.Fn (h, Xexp.App (Xexp.Var k, e)))
+  | Xexp.Handle (t, error_code, error) -> Xexp.Fn (k, Xexp.Fn (h, Xexp.App (Xexp.Var k, e)))
+
+(* TODO : Implement this function *)
+let removeExn : xexp -> xexp = fun e ->
+  let x = new_name () in
+  let y = new_name () in
+  Xexp.App (Xexp.App (removeExnIter e, Xexp.Fn (x, Var x)), Xexp.Fn (y, Num 201912))
