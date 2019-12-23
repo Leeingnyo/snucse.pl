1092431e3ab6df8aa6047a860ce1de8349a75c5b
Leeingnyo <dyd1928@naver.com>
2019-12-23 10:56:08 +0900

    add comments

--- homework06/SM5_skel/translate.ml
+++ homework06/SM5_skel/translate.ml
@@ -9,6 +9,7 @@ open Sm5
 module Translator = struct
   (* TODO : complete this function  *)
   let rec trans : K.program -> Sm5.command = function
+    (* LETF를 제외한 나머지는 그대로이다 *)
     | K.UNIT -> [Sm5.PUSH (Sm5.Val Sm5.Unit)]
     | K.NUM i -> [Sm5.PUSH (Sm5.Val (Sm5.Z i))]
     | K.TRUE -> [Sm5.PUSH (Sm5.Val (Sm5.B true))]
@@ -26,22 +27,27 @@ module Translator = struct
     | K.LETV (x, e1, e2) ->
       trans e1 @ [Sm5.MALLOC; Sm5.BIND x; Sm5.PUSH (Sm5.Id x); Sm5.STORE] @
       trans e2 @ [Sm5.UNBIND; Sm5.POP]
-    | K.LETF (f, x, e1, e2) -> (
+    | K.LETF (f, x, e1, e2) -> ( (* 함수 번역할 예정 *)
+      (* trans' 의 의미: 테일콜이 나타날 수 있을 것이란 기대가 가능한 expr 를 번역 *)
       let rec trans' : K.program -> Sm5.command = fun k -> (match k with
-      | K.CALLV (f', arg_exp) -> (* 끝콜 *)
-        [Sm5.PUSH (Sm5.Id f'); Sm5.PUSH (Sm5.Id f')] @ trans arg_exp @ [Sm5.MALLOC; Sm5.TAILCALL]
-      | K.CALLR (f', arg_var) -> (* 끝콜 *)
-        [Sm5.PUSH (Sm5.Id f'); Sm5.PUSH (Sm5.Id f'); Sm5.PUSH (Sm5.Id arg_var); Sm5.LOAD; Sm5.PUSH (Sm5.Id arg_var); Sm5.TAILCALL]
       | K.IF (e_cond, e_true, e_false) ->
+        (* IF의 condition은 테일콜이 될 수 없으므로 trans로, 2개의 분기는 테일콜이 될 수 있으므로 trans'로 번역 *)
         trans e_cond @ [Sm5.JTR (trans' e_true, trans' e_false)]
       | K.SEQ (e1, e2) -> trans e1 @ [Sm5.POP] @ trans' e2
+        (* SEQ의 앞은 테일콜이 될 수 없으므로 trans, 뒤는 될 수 있으므로 trans'로 번역 *)
       | K.LETV (x, e1, e2) ->
         trans e1 @ [Sm5.MALLOC; Sm5.BIND x; Sm5.PUSH (Sm5.Id x); Sm5.STORE] @
         trans' e2 @ [Sm5.UNBIND; Sm5.POP]
+        (* LET의 in 뒤는 테일콜이 될 수 있으므로 trans'로 번역 *)
       | K.LETF (f, x, e1, e2) ->
         [Sm5.PUSH (Sm5.Fn (x, [Sm5.BIND f] @ trans' e1 @ [Sm5.UNBIND; Sm5.POP])); Sm5.BIND f] @
         trans' e2 @ [Sm5.UNBIND; Sm5.POP]
-      | _ -> trans k
+        (* 함수 내부와 LET의 in 뒤는 테일콜이 될 수 있으므로 trans'로 번역 *)
+      | K.CALLV (f', arg_exp) -> (* 정말로 테일콜이므로 테일콜로 번역 *)
+        [Sm5.PUSH (Sm5.Id f'); Sm5.PUSH (Sm5.Id f')] @ trans arg_exp @ [Sm5.MALLOC; Sm5.TAILCALL]
+      | K.CALLR (f', arg_var) -> (* 정말로 테일콜이므로 테일콜로 번역 *)
+        [Sm5.PUSH (Sm5.Id f'); Sm5.PUSH (Sm5.Id f'); Sm5.PUSH (Sm5.Id arg_var); Sm5.LOAD; Sm5.PUSH (Sm5.Id arg_var); Sm5.TAILCALL]
+      | _ -> trans k (* 나머지 경우는 테일콜이 나타날 수 없으므로 원래의 번역함수로 번역 *)
       )
       in
       [Sm5.PUSH (Sm5.Fn (x, [Sm5.BIND f] @ trans' e1 @ [Sm5.UNBIND; Sm5.POP])); Sm5.BIND f] @
